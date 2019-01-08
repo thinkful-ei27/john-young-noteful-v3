2d2ee3ee13dac5bce427dadb8e365eb4c2470446
John Young <john.young@spreadstreet.io>
2019-01-08 16:04:47 -0500

    Updated all routes

--- scratch/queries.js
+++ scratch/queries.js
@@ -31,79 +31,79 @@ mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
     console.error(err);
   });
 
-// mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
-//   .then(() => {
-//     // template goes here
-//     let id = '111111111111111111111105';
-//     return Note.find({_id: id}).sort({updatedAt: 'desc'});
-//   })
-//   .then(results => {
-//     console.log(results);
-//   })
-//   .then(() => {
-//     return mongoose.disconnect();
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
+  .then(() => {
+    // template goes here
+    let id = '111111111111111111111105';
+    return Note.find({_id: id}).sort({updatedAt: 'desc'});
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
 
-// mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
-//   .then(() => {
-//     // template goes here
-//     const newNote = {
-//       title: 'A brand new title',
-//       content: 'With some brand new content'
-//     };
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
+  .then(() => {
+    // template goes here
+    const newNote = {
+      title: 'A brand new title',
+      content: 'With some brand new content'
+    };
 
-//     return Note.create(newNote);
-//   })
-//   .then(results => {
-//     console.log(results);
-//   })
-//   .then(() => {
-//     return mongoose.disconnect();
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+    return Note.create(newNote);
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
 
-// mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
-//   .then(() => {
-//     // template goes here
-//     const noteToUpdate = {_id: '5c34ff8523a64e572c3bec86'};
-//     const updateNote = {
-//       content: 'With some brand new content'
-//     };
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
+  .then(() => {
+    // template goes here
+    const noteToUpdate = {_id: '5c34ff8523a64e572c3bec86'};
+    const updateNote = {
+      content: 'With some brand new content'
+    };
 
-//     return Note.update(noteToUpdate, updateNote);
-//   })
-//   .then(results => {
-//     console.log(results);
-//   })
-//   .then(() => {
-//     return mongoose.disconnect();
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+    return Note.update(noteToUpdate, updateNote);
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
 
-// mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
-//   .then(() => {
-//     // template goes here
-//     const noteToDel = {_id: '5c34ff8523a64e572c3bec86'};
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
+  .then(() => {
+    // template goes here
+    const noteToDel = {_id: '5c34ff8523a64e572c3bec86'};
 
-//     return Note.deleteOne(noteToDel);
-//   })
-//   .then(results => {
-//     console.log(results);
-//   })
-//   .then(() => {
-//     return mongoose.disconnect();
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+    return Note.deleteOne(noteToDel);
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
