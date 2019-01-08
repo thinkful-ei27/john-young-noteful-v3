cd6c685d308a9531b543b2ca14356ecc029c896b
John Young <john.young@spreadstreet.io>
2019-01-08 14:33:56 -0500

    Added note model and schema

---
+++ models/note.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: { type: String, required: true},
+  content: String
+});
+
+// Add 'createdAt' and 'updatedAt' fields
+noteSchema.set('timestamps', true);
+
+module.exports = mongoose.model('Note', noteSchema);
